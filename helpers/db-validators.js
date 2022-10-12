8d62b4d7661b022fc856a27f16c080bb07b3d586
luciano rivera <lucianonicolasrivera8@gmail.com>
2022-10-12 15:22:57 -0300

    terminamos categorias

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,6 +1,9 @@
+const Categoria = require("../models/categoria");
 const Role = require("../models/role");
 
 const Usuario = require("../models/usuario")
+
+
 //  validando si ROLE existe en la base de datos
 
 const esRoleValido = async (role = "") => {
@@ -11,6 +14,7 @@ const esRoleValido = async (role = "") => {
     }
   }
  
+
  //validar si email existe
  
  const emailExiste =async(email)=>{
@@ -20,6 +24,8 @@ const esRoleValido = async (role = "") => {
         throw new Error(`El correo ${email} ya existe en la BD`);
     };
     }
+
+
     // validar ruta put :id existe en la base de datos 
 const idExiste =async(id)=>{
     
@@ -29,8 +35,20 @@ const idExiste =async(id)=>{
     };
     }
 
+
+    // validar categoria por id
+    const categoriaExiste =async(id)=>{
+    
+        const existeCategoria = await Categoria.findById({id});
+        if (!existeCategoria ) {
+            throw new Error(`El id ${id} no existe en la Base de Datos`);
+        };
+        }
+    
+
     module.exports = {
         esRoleValido,
         emailExiste,
-        idExiste
+        idExiste,
+        categoriaExiste 
     }
--- models/Server.js
+++ models/Server.js
@@ -6,6 +6,7 @@ class Server {
     this.port =process.env.PORT
     this.usuariosPath ="/api/usuarios"
     this.authPath ="/api/auth"
+    this.categoriasPath ="/api/categorias"
     //conectar db
     this.conectarDb();
     
@@ -27,6 +28,7 @@ class Server {
   routes() {
 this.app.use(this.usuariosPath, require("../routes/usuarios"))
 this.app.use(this.authPath, require("../routes/auth"))
+this.app.use(this.categoriasPath, require("../routes/categorias"))
   
     
 
